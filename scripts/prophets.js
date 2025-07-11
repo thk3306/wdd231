18ee7f5ae9a842e91cfa37e877787e5adc28baa0
thk3306 <rodingemil@gmail.com>
2025-07-11 16:10:02 +1200

    made prophets page

---
+++ scripts/prophets.js
@@ -0,0 +1,32 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const birthDate = document.createElement('p');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '300');
+        birthDate.textContent = `Born: ${prophet.birthdate} in ${prophet.birthplace}`;
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(birthDate);
+        cards.appendChild(card);
+    })
+}
+
+getProphetData();
