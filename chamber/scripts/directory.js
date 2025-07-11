00c902e392739d882b8a69aa69e13a9766919f71
thk3306 <rodingemil@gmail.com>
2025-07-11 17:19:53 +1200

    updated directory website with nz companies

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,3 +6,62 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+const url = 'https://thk3306.github.io/wdd231/chamber/data/members.json';
+
+const cards = document.querySelector('#cards');
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        const card = document.createElement('section');
+        const name = document.createElement('h2');
+        const image = document.createElement('img');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const website = document.createElement('a');
+        const membershipLevel = document.createElement('p');
+        const description = document.createElement('p');
+        const contactEmail = document.createElement('p');
+        name.textContent = member.name;
+        image.setAttribute('src', `images/${member.image}`);
+        image.setAttribute('alt', `Image of ${member.name}`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '200');
+        image.setAttribute('height', '300');
+        address.textContent = `Address: ${member.address}`;
+        phone.textContent = `Phone: ${member.phone}`;
+        website.setAttribute('href', member.website);
+        website.textContent = `Website: ${member.website}`;
+        membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
+        description.textContent = `Description: ${member.description}`;
+        contactEmail.textContent = `Contact Email: ${member.contactEmail}`;
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(membershipLevel);
+        card.appendChild(description);
+        card.appendChild(contactEmail);
+        cards.appendChild(card);
+    })
+}
+getMemberData();
+
+const gridButton = document.querySelector('#grid');
+const listButton = document.querySelector('#list');
+
+gridButton.addEventListener('click', () => {
+    cards.classList.remove('list');
+    cards.classList.add('grid');
+});
+
+listButton.addEventListener('click', () => {
+    cards.classList.remove('grid');
+    cards.classList.add('list');
+});
--- scripts/prophets.js
+++ scripts/prophets.js
@@ -19,8 +19,6 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '200');
-        portrait.setAttribute('height', '300');
         birthDate.textContent = `Born: ${prophet.birthdate} in ${prophet.birthplace}`;
         card.appendChild(fullName);
         card.appendChild(portrait);
